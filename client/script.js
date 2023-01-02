6e9ff75520fb85485380daa1ce7d14317e9ac47a
CodeCoast360 <danieladde9@gmail.com>
2023-01-02 05:39:43 +0000

    update the server url

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
 
     //! Fetch data from server => bot response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codezila.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
